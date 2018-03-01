fb11cfc4f7ba1e7c4608b43c0eb3a6e3c184784a
Jojo Linatoc <linatocjojop@gmail.com>
2018-03-01 11:49:37 +0800

    add page for AFAF

---
+++ app/controllers/australian.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+/**
+ * Created by PB5N0182 on 3/1/2018.
+ */
+public class australian extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+}
